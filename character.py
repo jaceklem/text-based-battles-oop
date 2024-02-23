17a68c75b416bed958d9c50ab71151f6717bab9d
jaceklem <47326974+jaceklem@users.noreply.github.com>
2024-02-23 17:45:09 +0100

    Add character.py with basic information

---
+++ character.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self, name: str, health: int, damage: int):
+        self.name = name
+        self.health = health
+        self.health_max = health
+        self.damage = damage
+
+    def attack(self, target):
+        target.hp -= self.damage
+        target.hp = max(target.hp, 0)
