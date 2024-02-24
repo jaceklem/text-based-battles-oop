cdbd831debc2d8394d6eae757e502e5125e40c1f
jaceklem <47326974+jaceklem@users.noreply.github.com>
2024-02-24 18:11:03 +0100

    Update character.py with Hero and Enemy class

--- character.py
+++ character.py
@@ -1,10 +1,40 @@
+from weapon import *
+
 class Character:
-    def __init__(self, name: str, health: int, damage: int):
+    def __init__(self, name: str, health: int):
         self.name = name
         self.health = health
         self.health_max = health
-        self.damage = damage
+
+        self.weapon = fists
 
     def attack(self, target):
-        target.health -= self.damage
+        target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+
+        print(f"{self.name} dealt {self.weapon.damage} damage to {target.name} using {self.weapon.name}")
+
+
+class Hero(Character):
+    def __init__(self, name: str, health: int):
+        super().__init__(name=name, health=health)
+        
+        self.default_weapon = self.weapon
+
+    def equip(self, weapon: str) -> None:
+        self.weapon = weapon
+
+        print(f"{self.name} equipped a(n) {self.weapon.name}")
+
+    def drop(self) -> None:
+        print(f"{self.name} dropped {self.weapon}!")
+
+        self.weapon = self.default_weapon
+
+
+
+class Enemy(Character):
+    def __init__(self, name: str, health: int, weapon: str) -> None:
+        super().__init__(name=name, health=health)
+
+        self.weapon = weapon
