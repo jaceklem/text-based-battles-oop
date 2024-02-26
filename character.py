7f953feb7ba06d5b8f245a6388900d52ce07570a
jaceklem <47326974+jaceklem@users.noreply.github.com>
2024-02-26 14:40:37 +0100

    Implemented functionality of health_bar.py

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 from weapon import *
+from health_bar import HealthBar
 
 class Character:
     def __init__(self, name: str, health: int):
@@ -11,6 +12,7 @@ class Character:
     def attack(self, target):
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+        target.health_bar.update()
 
         print(f"{self.name} dealt {self.weapon.damage} damage to {target.name} using {self.weapon.name}")
 
@@ -20,6 +22,7 @@ class Hero(Character):
         super().__init__(name=name, health=health)
         
         self.default_weapon = self.weapon
+        self.health_bar = HealthBar(self, color='green')
 
     def equip(self, weapon: str) -> None:
         self.weapon = weapon
@@ -38,3 +41,4 @@ class Enemy(Character):
         super().__init__(name=name, health=health)
 
         self.weapon = weapon
+        self.health_bar = HealthBar(self, color='red')
