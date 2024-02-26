03fb54fb61e27d80ea6f2055114b39bc5606815e
jaceklem <47326974+jaceklem@users.noreply.github.com>
2024-02-26 15:02:23 +0100

    Add swap mechanism

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from character import Hero, Enemy
 from weapon import *
 
 def space():
+    """Function that inputs a space between two lines
+    """
     print()
+
+### WEAPON SELECTION ###
+#    iron_sword
+#    short_bow
+#    fists
 
 # Small welcome screen
 print("Type 'exit' or 'q' to quit.")
@@ -12,13 +19,13 @@ space()
 hero = Hero(name="Hero", health=100)
 hero.equip(iron_sword)
 
-space()
-
 # Enemy properties
 enemy = Enemy(name="Enemy", health=60, weapon=short_bow)
 
 while True:
+    space()
     print("-" * 6, "Damage Indicator", "-" * 6)
+
     hero.attack(enemy)
     enemy.attack(hero)
 
@@ -30,5 +37,23 @@ while True:
 
     inp = input()
 
-    if inp == "exit" or inp == "q":
+    if inp.lower() == "swap":
+        hero.drop()
+
+        new_weapon = input("Choose your new weapon: ")
+        space()
+
+        if new_weapon.lower() == "sword":
+            hero.equip(iron_sword)
+            continue
+
+        if new_weapon.lower() == "bow":
+            hero.equip(short_bow)
+            continue
+
+        if new_weapon.lower() == "":
+            hero.equip(fists)
+            continue
+
+    if inp.lower() == "exit" or inp.lower() == "q":
         break
