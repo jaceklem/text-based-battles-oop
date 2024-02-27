bf58a749602bb4cdadcd4bcb1c007193e61e836f
jaceklem <47326974+jaceklem@users.noreply.github.com>
2024-02-27 11:14:54 +0100

    Add random damage integer

--- weapon.py
+++ weapon.py
@@ -1,22 +1,42 @@
+import random
 class Weapon:
-    def __init__(self, name: str, weapon_type: str, damage: int, value: int) -> None:
+    def __init__(self, 
+                 name: str, 
+                 weapon_type: str, 
+                 damage: int, 
+                 value: int) -> None:
         self.name = name
         self.weapon_type = weapon_type
         self.damage = damage
         self.value = value
 
+    def update_damage(self) -> None:
+        if self.name == "Iron Sword":
+            self.damage = random.randint(1, 5)
+
+        elif self.name == "Short Bow":
+            self.damage = random.randint(2, 4)
+
+        else:
+            self.damage = random.randint(0, 2)
 
 iron_sword = Weapon(name="Iron Sword", 
                     weapon_type="sharp", 
-                    damage=5, 
+                    damage=random.randint(1, 5), 
                     value=10)
 
 short_bow = Weapon(name="Short Bow",
                    weapon_type="ranged",
-                   damage=4,
+                   damage=random.randint(2, 4),
                    value=8)
 
 fists = Weapon(name="Fists",
                weapon_type="blunt",
-               damage=2,
+               damage=random.randint(0, 2),
                value=0)
+
+# Update the damage
+def dmg_update():
+    iron_sword.update_damage()
+    short_bow.update_damage()
+    fists.update_damage()
